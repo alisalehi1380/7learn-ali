4a06264486df7bac8ef4c43db186c816f967d8c6
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-04 22:10:35 +0330

    add(middleware): improve CheckControllerVersion

--- app/Http/Middleware/CheckControllerVersion.php
+++ app/Http/Middleware/CheckControllerVersion.php
@@ -9,17 +9,30 @@ use Symfony\Component\HttpFoundation\Response;
 class CheckControllerVersion
 {
     private const HTTP_NOT_FOUND = 404;
-    
+
     public function handle(Request $request, Closure $next): Response
     {
-        $controller = $request->route()->getController();
-        
-        $version = $request->route('version') ?: 'v1';
-        
-        if (!method_exists($controller, $version)) {
-            abort(self::HTTP_NOT_FOUND);
+        $method = $request->route()->getActionMethod();
+        $currentApiVersion = strtoupper($request->segment(2));
+        $controllerName = $request->route()->getControllerClass();
+        $controller = "App\Http\Controllers\API\\$currentApiVersion\\$controllerName";
+
+        if (method_exists($controller, $method)) {
+            $request->route()->uses("$controller@$method");
+            return $next($request);
         }
-        
-        return $next($request);
+
+        $numberApiVersion = (int)substr($currentApiVersion, 1);
+        if ($numberApiVersion > 1) {
+            $pastApiVersion = 'V' . ($numberApiVersion - 1);
+            $pastApiController = str_replace($currentApiVersion, $pastApiVersion, $controller);
+            if (method_exists($pastApiController, $method)) {
+                $request->route()->uses("$pastApiController@$method");
+                return $next($request);
+            }
+        }
+
+        abort(self::HTTP_NOT_FOUND);
     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+//use App\Http\Controllers\API\V2\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('check.controller.version')->group(function () {
+    Route::get('v2/posts', [PostController::class, 'index']);
+    Route::get('v2/posts/{post}', [PostController::class, 'show']);
 });
