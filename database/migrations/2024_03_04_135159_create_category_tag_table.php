0e971752a73a50856f3c8a41c019c55492ebae9c
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-04 18:56:46 +0330

    add(migration): posts, tags, category

---
+++ database/migrations/2024_03_04_135159_create_category_tag_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('category_tag', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('category_tag');
+    }
+};
