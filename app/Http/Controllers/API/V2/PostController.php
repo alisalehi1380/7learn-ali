c31043d2571bd2c0e74dfe414145bcd1207346e3
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-04 18:58:44 +0330

    add(controller): post api

---
+++ app/Http/Controllers/API/V2/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\V2;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+    
+    }
+}
