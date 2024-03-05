58d51b710d29a34f193533e77f8da11201a889d6
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-05 14:29:28 +0330

    add(test): ArrayPairFinder helper

--- tests/Unit/Helpers/ArrayPairFinderTest.php
+++ tests/Unit/Helpers/ArrayPairFinderTest.php
@@ -1,6 +1,8 @@
 <?php
 
 
+namespace Helpers;
+
 use App\Helpers\ArrayPairFinder;
 use PHPUnit\Framework\TestCase;
 
@@ -8,21 +10,21 @@ class ArrayPairFinderTest extends TestCase
 {
     public function test_findPair(): void
     {
-        $array = [1,2,3,4,5];
+        $array = [1, 2, 3, 4, 5];
         $target = 8;
         $exepted = [4, 5];
         $result = ArrayPairFinder::findPair($array, $target);
-
+        
         $this->assertEquals($exepted, $result);
     }
-
+    
     public function test_findPair_unordered_numbers(): void
     {
-        $array = [2,1,4,3,5];
+        $array = [2, 1, 4, 3, 5];
         $target = 8;
         $exepted = [4, 5];
         $result = ArrayPairFinder::findPair($array, $target);
-
+        
         $this->assertEquals($exepted, $result);
     }
 }
