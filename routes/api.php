28d55c2737c1634447e0c1246d5cf5766eecd4ea
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-05 14:30:25 +0330

    add(route): api.post.index name

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-//use App\Http\Controllers\API\V2\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('check.controller.version')->group(function () {
-    Route::get('v2/posts', [PostController::class, 'index']);
+    Route::get('v2/posts', [PostController::class, 'index'])->name('api.post.index');
     Route::get('v2/posts/{post}', [PostController::class, 'show']);
     Route::get('v1/posts', [PostController::class, 'index']);
 });
